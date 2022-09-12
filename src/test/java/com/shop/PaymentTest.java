b6e5b5be73c8ae68e7e75a0a50bd6d01a596ef47
adrianpusty <adrianpusty>
2022-09-12 15:54:08 +0200

    Formatting changed in PaymentTest class

--- src/test/java/com/shop/PaymentTest.java
+++ src/test/java/com/shop/PaymentTest.java
@@ -1,5 +1,7 @@
 package com.shop;
 
+import org.junit.jupiter.api.Test;
+
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
@@ -7,25 +9,41 @@ import java.util.List;
 import static com.shop.CreditCardType.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class PaymentTest {
+class PaymentTest
+{
 
-    @org.junit.jupiter.api.Test
-    void withDiscount()
+    public static final Product bike = Product.builder().name("bike")
+            .price(BigDecimal.valueOf(2000)).build();
+    public static final Product steamGiftCard = Product.builder().name("steam gift card")
+            .price(BigDecimal.valueOf(50)).build();
+    public static final Product amazonGiftCard = Product.builder().name("amazon gift card")
+            .price(BigDecimal.valueOf(100)).build();
+    public static final Product bubbleGum = Product.builder().name("bubble gum")
+            .price(BigDecimal.valueOf(0.55)).build();
+    public static final Product car = Product.builder().name("toyota car")
+            .price(BigDecimal.valueOf(10_000)).build();
+    public static final Product window = Product.builder().name("window")
+            .price(BigDecimal.valueOf(1500)).build();
+
+    @Test
+    void normalTest()
     {
-        Product bike = Product.builder().name("bike").price(BigDecimal.valueOf(2000)).build();
-        Product steamGiftCard = Product.builder().name("steam gift card").price(BigDecimal.valueOf(50)).build();
-        Product amazonGiftCard = Product.builder().name("amazon gift card").price(BigDecimal.valueOf(100)).build();
-        Product bubbleGum = Product.builder().name("bubble gum").price(BigDecimal.valueOf(0.55)).build();
-        Product car = Product.builder().name("toyota car").price(BigDecimal.valueOf(10_000)).build();
-        Product window = Product.builder().name("window").price(BigDecimal.valueOf(1500)).build();
+        List<Product> products = Arrays.asList(steamGiftCard, amazonGiftCard, bubbleGum, car, window);
+        testDiscount(products, NORMAL, new BigDecimal("11650.55"));
+    }
 
-        List<Product> products1 = Arrays.asList(bike, steamGiftCard, amazonGiftCard, bubbleGum);
-        List<Product> products2 = Arrays.asList(steamGiftCard, amazonGiftCard, bubbleGum, car, window);
-        List<Product> products3 = Arrays.asList(steamGiftCard, bubbleGum);
+    @Test
+    void silverTest()
+    {
+        List<Product> products = Arrays.asList(bike, steamGiftCard, amazonGiftCard, bubbleGum);
+        testDiscount(products, SILVER, new BigDecimal("1935.5"));
+    }
 
-        testDiscount(products1, SILVER, new BigDecimal("1935.5"));
-        testDiscount(products2, NORMAL, new BigDecimal("11650.55"));
-        testDiscount(products3, GOLD, new BigDecimal("40.44"));
+    @Test
+    void goldTest()
+    {
+        List<Product> products = Arrays.asList(steamGiftCard, bubbleGum);
+        testDiscount(products, GOLD, new BigDecimal("40.44"));
     }
 
     private void testDiscount(List<Product> products, CreditCardType creditCardType, BigDecimal expected)
